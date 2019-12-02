003b7115d389e4ec7a6aa945e8103f9534cab80c
patricio.diaz <patricio.diaz@PDIAZ-NOTE.bna.intranet>
2019-12-02 09:49:25 -0300

    Added on 02122019 / 09:49

--- Test Suites/MODULO EDITAR/CREAR NOTICIAS.ts
+++ Test Suites/MODULO EDITAR/CREAR NOTICIAS.ts
@@ -14,78 +14,78 @@
       <guid>a79a716f-6abd-45d8-b6d1-856e28cf79af</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MODULO EDITAR/CREAR NOTICIAS - ENTREVISTA - RECOMENDADO</testCaseId>
+      <testCaseId>Test Cases/CMS - MODULO EDITAR/CREAR NOTICIAS - ENTREVISTA - RECOMENDADO</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>2412e835-18f6-4718-a644-da68407901c9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MODULO EDITAR/CREAR NOTICIAS - NATIVE - NO RECOMENDADO</testCaseId>
+      <testCaseId>Test Cases/CMS - MODULO EDITAR/CREAR NOTICIAS - NATIVE - NO RECOMENDADO</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>3ea9abb7-df48-497d-a6ef-adbe42a92c55</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MODULO EDITAR/CREAR PROYECTOS - ESTANDAR - REVISADO</testCaseId>
+      <testCaseId>Test Cases/CMS - MODULO EDITAR/CREAR PROYECTOS - ESTANDAR - REVISADO</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>6b48bbb5-da21-4855-9033-4e1de76a5ccb</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MODULO EDITAR/CREAR PROYECTOS - PRA</testCaseId>
+      <testCaseId>Test Cases/CMS - MODULO EDITAR/CREAR PROYECTOS - PRA</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>4a500397-b671-41fb-a2ad-4401fee7b854</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MODULO EDITAR/CREAR PROYECTOS</testCaseId>
+      <testCaseId>Test Cases/CMS - MODULO EDITAR/CREAR PROYECTOS</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>a5266ae4-c0d9-494e-a5ad-280b174b1fbd</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MODULO EDITAR/CREAR HITOS - ESTANDAR</testCaseId>
+      <testCaseId>Test Cases/CMS - MODULO EDITAR/CREAR HITOS - ESTANDAR</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>ca84651d-ed25-4717-8b8a-4f0ed7be7c57</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MODULO EDITAR/CREAR HITOS</testCaseId>
+      <testCaseId>Test Cases/CMS - MODULO EDITAR/CREAR HITOS</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>d99bb1f3-8f5a-4f16-9dbd-0d45efdc0ee4</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MODULO EDITAR/CREAR COMPANY - ESTANDAR - REVISADO</testCaseId>
+      <testCaseId>Test Cases/CMS - MODULO EDITAR/CREAR COMPANY - ESTANDAR - REVISADO</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>9a9234fa-341b-4e18-b850-3095d4b18200</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MODULO EDITAR/CREAR COMPANY</testCaseId>
+      <testCaseId>Test Cases/CMS - MODULO EDITAR/CREAR COMPANY</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>554f02c4-1549-425b-9e6f-189baecfcc81</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MODULO EDITAR/CREAR PERSONAS - ESTANDAR - REVISADO - ACCESO ESPECIAL</testCaseId>
+      <testCaseId>Test Cases/CMS - MODULO EDITAR/CREAR PERSONAS - ESTANDAR - REVISADO - ACCESO ESPECIAL</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>fbaf1978-b644-461a-ba8c-d57f286fdb94</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MODULO EDITAR/CREAR PERSONAS</testCaseId>
+      <testCaseId>Test Cases/CMS - MODULO EDITAR/CREAR PERSONAS</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>7d43c1ee-fbdd-4997-8077-383c460d1226</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MODULO EDITAR/CREAR REPORTES</testCaseId>
+      <testCaseId>Test Cases/CMS - MODULO EDITAR/CREAR REPORTES</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>3e63be24-9dba-495d-8bf6-59f16b932ad9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/MODULO EDITAR/CREAR SET DE DATOS</testCaseId>
+      <testCaseId>Test Cases/CMS - MODULO EDITAR/CREAR SET DE DATOS</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
